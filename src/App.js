96f83090725989b9a6af31f2c62ddfb1ffb95451
yurisokolovicz <yurisokolovicz@gmail.com>
2023-04-18 11:11:03 -0300

    Handling Http States & Feedback with Redux

--- src/App.js
+++ src/App.js
@@ -1,29 +1,73 @@
-import { useEffect } from 'react'; // To watch for changes in our cart state
-import { useSelector } from 'react-redux';
+import { Fragment, useEffect } from 'react'; // use Effect to watch for changes in our cart state
+import { useSelector, useDispatch } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { uiActions } from './store/ui-slice';
+import Notification from './components/UI/Notification';
+
+let isInitial = true;
 
 function App() {
+    const dispatch = useDispatch();
     const showCart = useSelector(state => state.ui.cartIsVisible);
     const cart = useSelector(state => state.cart);
+    const notification = useSelector(state => state.ui.notification);
 
     useEffect(() => {
-        // The PUT method replaces the entire resource with the request payload. The difference to POST is that PUT is idempotent: calling it once or several times successively has the same effect (that is no side effect), where successive identical POST may each have additional effects, like passing an order several times.
-        fetch('https://react-http-f7e2d-default-rtdb.firebaseio.com/cart.json', { method: 'PUT', body: JSON.stringify(cart) });
-    }, [cart]);
-    // We add cart as dependency to useEffect because we want to watch for changes in our cart state.
+        const sendCartData = async () => {
+            dispatch(
+                uiActions.showNotification({
+                    status: 'pending',
+                    title: 'Sending...',
+                    message: 'Sending cart data!'
+                })
+            );
+            const response = await fetch('https://react-http-f7e2d-default-rtdb.firebaseio.com/cart.json', {
+                method: 'PUT',
+                body: JSON.stringify(cart)
+            });
+
+            if (!response.ok) {
+                throw new Error('Sending cart data failed.');
+            }
+
+            dispatch(
+                uiActions.showNotification({
+                    status: 'success',
+                    title: 'Success!',
+                    message: 'Sent cart data successfully!'
+                })
+            );
+        };
+
+        if (isInitial) {
+            isInitial = false;
+            return;
+        }
+
+        sendCartData().catch(error => {
+            dispatch(
+                uiActions.showNotification({
+                    status: 'error',
+                    title: 'Error!',
+                    message: 'Sending cart data failed!'
+                })
+            );
+        });
+    }, [cart, dispatch]);
 
     return (
-        <Layout>
-            {showCart && <Cart />}
-            <Products />
-        </Layout>
+        <Fragment>
+            {notification && <Notification status={notification.status} title={notification.title} message={notification.message} />}
+            <Layout>
+                {showCart && <Cart />}
+                <Products />
+            </Layout>
+        </Fragment>
     );
 }
 
 export default App;
 
-// The useSelector sets up a subscription to Redux. SO whenever the state changes (Redux store change), the component will re-render and we will get the latest state (In this case, the latest cart). The useEffect hook will run after every component re-render cycle. So we can use it to send.
-
-// AND WE CAN KEEP our logic for updating the cart inside of the reducer. Because we changed the order, we first update our Redux store and then we select the updated store to send the request We can do that by using the useEffect hook. The useEffect hook allows us to execute some code whenever a component re-renders. We can use it to send a Http reques
+// After creating a showNotification in the ui-slice we want to dispatch to show notification action when we start sending the data, when we are done with the data (already sent) and if we have an error.(we import useSispatch from react-redux). In the function App we code  const dispatch = useDispatch(). And we import our uiActions from store/ui-slice. And then in useEffect when we sent the cart data we initially dispatch UI action showNotification(). We also do it when we have an error and when we are done sending the data.
