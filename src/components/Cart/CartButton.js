164004d80ca985781ebe9aec52b1fe54887c2ad1
yurisokolovicz <yurisokolovicz@gmail.com>
2023-04-19 10:00:25 -0300

    Add media queries for smartphones

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -12,7 +12,7 @@ const CartButton = props => {
 
     return (
         <button className={classes.button} onClick={toggleCartHandler}>
-            <span>My Cart</span>
+            <span>Cart</span>
             <span className={classes.badge}>{cartQuantity}</span>
         </button>
     );
